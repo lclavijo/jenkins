3114d5ad7ec482939f04d284bf7a54c2fb140f5c
lclavijo <leonardo_clavijo@hotmail.com>
2021-06-16 17:21:59 -0500

    Add files via upload

---
+++ resolve-env.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('jwt-token-SANTANDER-DEV.txt'),
+    output: process.stdout,
+    console: false
+});
+
+readInterface.on('line', function(line) {
+    let newToken = line;
+
+    let rawdata = fs.readFileSync('postman\\Development.postman_environment.json');
+    let env = JSON.parse(rawdata);
+    let token = env.values.find(item => item.key === "token");
+    token.value = newToken;
+
+    let data = JSON.stringify(env);
+    fs.writeFileSync('dev-env.json', data);
+});
